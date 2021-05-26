c952e902ba35b0f1c0e83bd1ff5881eb9dccf82a
etienne <etnnth@gmail.com>
2021-05-26 15:04:02 +0200

    start working on raifml

--- 1522A/main.py
+++ 1522A/main.py
@@ -1,1 +1,19 @@
 # Entry point
+
+import predict
+
+
+number_of_test_case = int(input())
+
+for i in range(number_of_test_case):
+    if i > 0:
+        previous_results = input().split()
+    current_match = input().split()
+    division = int(current_match[0])
+    match_time= current_match[1]
+    home_team_id = int(current_match[2])
+    away_team_id = int(current_match[3])
+    referee_id = int(current_match[4])
+    coeffs = [float(coeff) for coeff in current_match[5:]]
+    print(coeffs)
+    print(predict.predict(division,0,0, coeffs))
---
+++ 1522A/predict.py
@@ -0,0 +1,4 @@
+
+
+def predict(division, home_team, away_team, coeffs):
+    return "DRAW"
